04b72d64696fd43d3b3f84bd1fa962a5ef9ba8bf
Kilisi <kilisivazquez@yahoo.com>
2022-06-09 19:45:40 -0700

    youtube API

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,15 +1,12 @@
 // jp-will create and append the information from the api and place it in their 2 containers
-let youtube = {
-    "apiKey": null
-
-};
 
 // jp-will create and append the information from the api and place it in their 2 containers
 
 // jp-will create and append the information from the api and place it in their 2 containersgit p
 
 var rawgAPI = '4195cc8002804467be513fd2af860f7e';
-var youTubeAPI = "AIzaSyCKEJL7QBlhCvJotb_E4HkcyPhBFwFO2WU";
+var youtubeAPI = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=";
+var youtubeAPIKey = "&key=AIzaSyCKEJL7QBlhCvJotb_E4HkcyPhBFwFO2WU";
 // rawg link - https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
 // rawg link - https://api.rawg.io/api/games?key=4195cc8002804467be513fd2af860f7e&dates=2019-09-01,2019-09-30&platforms=18,1,7
 // onclick function to intiate the data pull from rawg and youtube
@@ -31,12 +28,17 @@ function rawgPull() {
     fetch('https://api.rawg.io/api/games?key=' + rawgAPI + '&dates=2019-09-01,2019-09-30&platforms=18,1,7')
         .then(response => response.json())
         .then(data => {
-            console.log(data)
-
+            console.log(data);
+            
         })
 };
 
 function youTubePull() {
+    fetch(youtubeAPI + "league%20of%20legends" + youtubeAPIKey)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
 
+    })
 
 };
