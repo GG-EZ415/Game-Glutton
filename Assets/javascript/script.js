20bf3dc9098aaebe3aa38f8a36edda6ac6ec9dda
Kilisi <kilisivazquez@yahoo.com>
2022-06-13 18:22:46 -0700

    new content

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,36 +1,50 @@
-// jp-will create and append the information from the api and place it in their 2 containers
+// JP-commented out YouTube stuff because I used up all out YouTube api fetches
 
-// jp-will create and append the information from the api and place it in their 2 containers
-
-// jp-will create and append the information from the api and place it in their 2 containersgit p
 var rawgAPI = '4195cc8002804467be513fd2af860f7e';
 var youtubeAPI = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=";
 var youtubeAPIKey = "&key=AIzaSyCKEJL7QBlhCvJotb_E4HkcyPhBFwFO2WU";
-// rawg link - https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
-// rawg link - https://api.rawg.io/api/games?key=4195cc8002804467be513fd2af860f7e&dates=2019-09-01,2019-09-30&platforms=18,1,7
-// onclick function to intiate the data pull from rawg and youtube
-// https://api.rawg.io/api/games
-// stores link https://api.rawg.io/api/games/{game_pk}/stores
+
 
 // get game game name on clickbar searchBarEl = "";
-$("button").on('click', function () {
+$("#search-button").on('click', function () {
     console.log('button');
     searchBarEl = $('#search-input').val().trim();
     var searchVal = searchBarEl.split(" ").join("%20");
     console.log(searchVal);
     rawgPull(searchVal);
+<<<<<<< HEAD
     youTubePull(searchVal);
+=======
+    $('#search-input').val("");
+    // moved youtube pull to rawg to account for mature game content
+>>>>>>> d8ece49c32075068040c73907cf78c90c991d235
 });
 
 
+function createCard(title, rating, meta, img) {
+    // this return is handing off the rest of the entire string
+    return '<div class="column is-3-tablet is-5-desktop">' +
+        '<div class="card">' +
+        '<div class="card-image" id="rawg-image">' +
+        '<img src="' + img + '" alt="' + title + '">' +
+        '</div>' +
+        '<div class="card-content">' +
+        '<p class="title is-size-5">' +
+        '<h1 class="has-text-centered" id="rawg-game-title">' + title + '</h1>' +
+        '</p>' +
+        '</div>' +
+        '<footer class="card-footer">' +
+        '<p class="card-footer-item has-text-danger-dark title has-text-weight-bold" id="rawg-esrb-rating">' + rating + '</p>' +
+        '<p class="card-footer-item has-text-warning-dark title has-text-weight-bold">' + meta + '</p>' +
+        '</footer>' +
+        '</div>' +
+        '</div>';
+}
+var testEl = document.querySelector('.test')
 
-// leon-you count potentially gull you top games and screenshots from here
-var $newdiv1 = $("<div id='object1'></div>"),
-    newdiv2 = document.createElement("div"),
-    existingdiv1 = document.getElementById("footer");
 
+// if else condtional for games not found
 function rawgPull(gameName) {
-    // will fetch ALL the games
     fetch('https://api.rawg.io/api/games?key=' + rawgAPI + '&search=' + gameName)
         .then(response => response.json())
         .then(data => {
@@ -39,15 +53,39 @@ function rawgPull(gameName) {
             console.log(data.results[0].esrb_rating.name);
             console.log(data.results[0].metacritic);
             console.log(data.results[0].background_image);
-            // console.log(data.results[0].stores);
-        })
-    $(".test").append($newdiv1, [newdiv2, existingdiv1]);
+            testEl.innerHTML = "";
+            // doesn't need for loop
+            for (var i = 0; i < 1; i++) {
+                var title = data.results[i].name;
+                var rating = data.results[i].esrb_rating.name;
+                var metacritic = data.results[i].metacritic;
+                var rawgPic = data.results[i].background_image;
+                $("#rawg-cont").append(createCard(title, rating, metacritic, rawgPic));
+            }
+            //coditional to determine wiether or not youtube api is called no if mature
+            if (rating === "Mature" && localStorage.getItem('modalAnswer') === 'false') {
+                console.log('you cant see that');
+                // perhaps add a photo
+            }
+            else {
+                // youTubePull(gameName);
+            }
 
+        })
 };
 
+// check query parameters to see top games (possibly game of the year titles)
 
 
+// function youTubePull(gameName) {
+//     fetch(youtubeAPI + gameName + youtubeAPIKey)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//             console.log(data.items[0].snippet.title);
+//             console.log(data.items[0].snippet.thumbnails.default.url);
 
+<<<<<<< HEAD
 function youTubePull(gameName) {
     fetch(youtubeAPI + gameName + youtubeAPIKey)
     .then(response => response.json())
@@ -63,9 +101,17 @@ function youTubePull(gameName) {
     })
 $('.test').append(youtubeDataCont);
 
+=======
+//         })
 
-};
+// };
+>>>>>>> d8ece49c32075068040c73907cf78c90c991d235
 
+// modal
+var modalBackgroundEl = document.querySelector(".modal-background");
+var modalEl = document.querySelector(".modal");
+
+<<<<<<< HEAD
 
 var youtubeDataCont = '<div class="column is-3-tablet is-5-desktop">' +
     '<div class="card">' +
@@ -98,3 +144,21 @@ var youtubeDataCont = '<div class="column is-3-tablet is-5-desktop">' +
 
 
 
+=======
+$(function () {
+    console.log("ready!");
+    $(modalEl).addClass("is-active")
+});
+// true & false easier and better practice
+$("#modal-yes").on('click', function () {
+    console.log('modal');
+    $(modalEl).removeClass("is-active")
+    localStorage.setItem('modalAnswer', 'true')
+});
+
+$("#modal-no").on('click', function () {
+    console.log('modal');
+    $(modalEl).removeClass("is-active")
+    localStorage.setItem('modalAnswer', 'false')
+});
+>>>>>>> d8ece49c32075068040c73907cf78c90c991d235
